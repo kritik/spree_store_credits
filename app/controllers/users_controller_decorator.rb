1b6e131adf1e3adbd18b687310f30d3438ff00d4
Andrew Purdy <andrewmp1@gmail.com>
2011-04-15 00:39:05 -0700

    Removing debugger statement

--- app/controllers/users_controller_decorator.rb
+++ app/controllers/users_controller_decorator.rb
@@ -4,7 +4,6 @@ UsersController.class_eval do
   private
   
   def find_orders_with_store_credit
-    debugger
     @orders_with_store_credit = @user.orders.joins(:adjustments).where(:adjustments => {:source_type => 'StoreCredit'})
   end
 end
